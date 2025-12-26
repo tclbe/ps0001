71793f7404540692b7243e4831f0009f157ca4ff
tclbe <ninetyseven98@gmail.com>
2025-12-26 20:58:06 +0800

    reorg, upload some notes

---
+++ tutorials_and_labs/1.1-variables_code.py
@@ -0,0 +1,41 @@
+#%% Welcome to Python!
+module = "PS0001"
+lab = 1
+author = "Caleb Tay"
+print("Hello, and welcome to", module, "week", lab)
+print("This lab was written and developed by", author)
+
+#%% The `print()` function
+print("Hello World!")
+"I run, but I do not print!"
+print("I take", "as many", "commas", "as you want!")
+print("I can also print numbers:", 1, 3, 5, 7, 9)
+
+#%% Variables
+变量_legal = 1
+print("variable 变量_legal contains the value", 变量_legal)
+
+#%% The *assignment* operator (`=`)
+3 + 4
+5 - 6
+7 * 8
+9 / 10
+
+#%%
+hands = 2
+print("It takes", hands, "hands to clap.")
+
+#%%
+print("It takes", 2, "hands to clap.")
+
+#%%
+a = 2
+print("It takes", a, "hands to clap.")
+
+#%%
+first = 3
+second = 5
+third = 7
+second = first * third
+third = second - 5
+
